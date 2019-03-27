9bebfbc6153644b7a82717977ece25ee3dfaa1a1
zzuutt <zzuutt@users.noreply.github.com>
2019-03-27 11:18:42 +0100

    Update update.sql

--- Config/update.sql
+++ Config/update.sql
@@ -1,3 +1,5 @@
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
 SET FOREIGN_KEY_CHECKS = 0;
 
 UPDATE `customer_validation_status_i18n` SET `locale` = 'en_US' WHERE `customer_validation_status_i18n`.`id` = 1 AND `customer_validation_status_i18n`.`locale` = 'en_EN';
